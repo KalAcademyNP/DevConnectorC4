36a093a2c8b7dcac048001f00f315986ac424c31
Kal <info@kalacademy.org>
2019-06-15 09:59:29 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//Db config
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = 5004;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
